e8f4ab669ae9a1e29527235e140791d990e9a8d9
Admin <Admin@DESKTOP-SRMBPCR>
2026-05-13 13:45:16 +0530

    model 2

--- Employee Details1/Program.cs
+++ Employee Details1/Program.cs
@@ -8,46 +8,70 @@ namespace Employee_Details1
 {
     public class Employee
     {
-        public static string CompanyName = "TCS";
-        public int Eid;
-        public string EmpName;
-        public double Salary;
+        //    public static string CompanyName = "TCS";
+        //    public int Eid;
+        //    public string EmpName;
+        //    public double Salary;
 
-        public void AcceptEmployeeDetails()
-        {
-            Console.WriteLine("Enter Employee ID :");
-            Eid=int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Employee Name :");
-            EmpName=Console.ReadLine();
-            Console.WriteLine("Enter Employee Salary :");
-            Salary=int.Parse(Console.ReadLine());
+        //    public void AcceptEmployeeDetails()
+        //    {
+        //        Console.WriteLine("Enter Employee ID :");
+        //        Eid=int.Parse(Console.ReadLine());
+        //        Console.WriteLine("Enter Employee Name :");
+        //        EmpName=Console.ReadLine();
+        //        Console.WriteLine("Enter Employee Salary :");
+        //        Salary=int.Parse(Console.ReadLine());
+        //    }
+        //    public static void displayCompany()
+        //    {
+        //        Console.WriteLine("Company name is :" + CompanyName);
+        //    }
+        //    public void DisplayEmployeeDetails()
+        //    {
+        //        Console.WriteLine(" Employee ID is :"+Eid);
+        //        Console.WriteLine(" Employee Name is :"+EmpName);
+        //        Console.WriteLine(" Employee Salary is :"+Salary);
+        //    }
         }
-        public static void displayCompany()
-        {
-            Console.WriteLine("Company name is :" + CompanyName);
-        }
-        public void DisplayEmployeeDetails()
-        {
-            Console.WriteLine(" Employee ID is :"+Eid);
-            Console.WriteLine(" Employee Name is :"+EmpName);
-            Console.WriteLine(" Employee Salary is :"+Salary);
-        }
-    }
+
+
+       
+
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Employee[] e = new Employee[5];
-            for(int i = 0; i < e.Length; i++)
+            //Employee[] e = new Employee[5];
+            //for(int i = 0; i < e.Length; i++)
+            //{
+            //    e[i]=new Employee();
+            //    e[i].AcceptEmployeeDetails();
+            //}
+            //for(int j=0; j < e.Length; j++)
+            //{
+            //    Employee.displayCompany();
+            //    e[j].DisplayEmployeeDetails();
+            //}
+
+            int[] eid = new int[2];
+            string[] ename = new string[2];
+            double[] Salary = new double[2];
+            for(int i=0; i<eid.Length; i++)
             {
-                e[i]=new Employee();
-                e[i].AcceptEmployeeDetails();
+                Console.WriteLine("Enter employee id :");
+                eid[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter employee name :");
+                ename[i] = Console.ReadLine();
+                Console.WriteLine("Enter employee salary :");
+                Salary[i] = int.Parse(Console.ReadLine());
             }
-            for(int j=0; j < e.Length; j++)
+            for(int j=0;j<eid.Length;j++)
             {
-                Employee.displayCompany();
-                e[j].DisplayEmployeeDetails();
+                Console.WriteLine("Employee id is : {0}" , eid[j]);
+                Console.WriteLine("Employee name is : {0}" , ename[j]);
+                Console.WriteLine("Employee salary is : {0}" , Salary[j]);
             }
         }
     }
